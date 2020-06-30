b029400b35961507330d1e8a12009c7982444e09
Zichu <527200054@qq.com>
2020-06-30 02:58:23 -0400

    1

---
+++ Duplicate Emails.sql
@@ -0,0 +1,26 @@
+-- Write a SQL query to find all duplicate emails in a table named Person.
+
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
+
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+# Write your MySQL query statement below
+SELECT Email
+FROM
+(
+    SELECT Email, COUNT(Email) as Number
+    FROM Person
+    GROUP BY Email) as statistic
+WHERE Number >1
+
+#TIP: Use COUNT and GROUP BY
